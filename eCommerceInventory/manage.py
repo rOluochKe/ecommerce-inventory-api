4a1272aa8a711ad56fd6f2ccb874ff3ab00e0d9c
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-04 17:10:48 +0300

    project initiation

--- eCommerceInventory/manage.py
+++ eCommerceInventory/manage.py
@@ -3,10 +3,18 @@
 import os
 import sys
 
+from eCommerceInventory.settings import base
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCommerceInventory.settings')
+    if base.DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eCommerceInventory.settings.local")
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "eCommerceInventory.settings.production"
+        )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
