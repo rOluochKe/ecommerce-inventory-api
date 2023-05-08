f5f58be4232fb970a9c09983c82c9ca1b7e5af2a
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-08 18:24:47 +0300

    added sub product inventory

--- eCommerceInventory/eCommerceInventory/product/models.py
+++ eCommerceInventory/eCommerceInventory/product/models.py
@@ -22,12 +22,29 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(
         "Category", on_delete=models.SET_NULL, null=True, blank=True
     )
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField(max_length=10)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="product_line",
+    )
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.sku)
--- eCommerceInventory/eCommerceInventory/product/views.py
+++ eCommerceInventory/eCommerceInventory/product/views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Brand, Category, Product
@@ -39,7 +40,32 @@ class ProductViewSet(viewsets.ViewSet):
 
     queryset = Product.objects.all()
 
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
+        return Response(serializer.data)
+
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+        url_name="all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
