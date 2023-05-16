58bad0b276af081e3c5b282d389c15443835dd4e
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-16 14:54:38 +0300

    updated end to end test

--- eCommerceInventory/eCommerceInventory/tests/product/test_endpoints.py
+++ eCommerceInventory/eCommerceInventory/tests/product/test_endpoints.py
@@ -9,24 +9,8 @@ class TestCategoryEndpoints:
     endpoint = "/api/category/"
 
     def test_category_get(self, category_factory, api_client):
-        # Arrange
-        category_factory.create_batch(4)
-        # Act
+        category_factory.create_batch(4, is_active=True)
         response = api_client().get(self.endpoint)
-        # Assert
-        assert response.status_code == 200
-        assert len(json.loads(response.content)) == 4
-
-
-class TestBrandEndpoints:
-    endpoint = "/api/brand/"
-
-    def test_brand_get(self, brand_factory, api_client):
-        # Arrange
-        brand_factory.create_batch(4)
-        # Act
-        response = api_client().get(self.endpoint)
-        # Assert
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 4
 
@@ -34,22 +18,13 @@ class TestBrandEndpoints:
 class TestProductEndpoints:
     endpoint = "/api/product/"
 
-    def test_return_all_products(self, product_factory, api_client):
-        # Arrange
-        product_factory.create_batch(4)
-        # Act
-        response = api_client().get(self.endpoint)
-        # Assert
-        assert response.status_code == 200
-        assert len(json.loads(response.content)) == 4
-
-    def test_return_single_product_by_name(self, product_factory, api_client):
+    def test_return_single_product_by_slug(self, product_factory, api_client):
         obj = product_factory(slug="test-slug")
         response = api_client().get(f"{self.endpoint}{obj.slug}/")
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 1
 
-    def test_return_products_by_category_name(
+    def test_return_products_by_category_slug(
         self, category_factory, product_factory, api_client
     ):
         obj = category_factory(slug="test-slug")
