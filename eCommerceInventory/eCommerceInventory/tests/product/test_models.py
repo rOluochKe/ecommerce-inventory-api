44d160ab551e27bdc40f20bfad10a699dad23cbe
Raymond Oluoch <rasholuoch@gmail.com>
2023-05-16 14:56:37 +0300

    updated unit test

--- eCommerceInventory/eCommerceInventory/tests/product/test_models.py
+++ eCommerceInventory/eCommerceInventory/tests/product/test_models.py
@@ -1,5 +1,8 @@
 import pytest
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+
+from eCommerceInventory.product.models import Category, Product
 
 pytestmark = pytest.mark.django_db
 
@@ -9,44 +12,104 @@ class TestCategoryModel:
         obj = category_factory(name="test_cat")
         assert obj.__str__() == "test_cat"
 
+    def test_name_max_length(self, category_factory):
+        name = "x" * 236
+        obj = category_factory(name=name)
+        with pytest.raises(ValidationError):
+            obj.full_clean()
 
-class TestBrandModel:
-    def test_str_output(self, brand_factory):
-        obj = brand_factory(name="test_brand")
-        assert obj.__str__() == "test_brand"
+    def test_slug_max_length(self, category_factory):
+        slug = "x" * 256
+        obj = category_factory(slug=slug)
+        with pytest.raises(ValidationError):
+            obj.full_clean()
+
+    def test_name_unique_field(self, category_factory):
+        category_factory(name="test_cat")
+        with pytest.raises(IntegrityError):
+            category_factory(name="test_cat")
+
+    def test_slug_unique_field(self, category_factory):
+        category_factory(slug="test_name")
+        with pytest.raises(IntegrityError):
+            category_factory(slug="test_name")
+
+    def test_is_active_false_default(self, category_factory):
+        obj = category_factory()
+        assert obj.is_active is False
+
+    def test_parent_category_on_delete_protect(self, category_factory):
+        obj1 = category_factory()
+        category_factory(parent=obj1)
+        with pytest.raises(IntegrityError):
+            obj1.delete()
+
+    def test_parent_field_null(self, category_factory):
+        obj1 = category_factory()
+        assert obj1.parent is None
+
+    def test_return_category_active_only_true(self, category_factory):
+        category_factory(is_active=True)
+        category_factory(is_active=False)
+        qs = Category.objects.is_active().count()
+        assert qs == 1
+
+    def test_return_category_active_only_false(self, category_factory):
+        category_factory(is_active=True)
+        category_factory(is_active=False)
+        qs = Category.objects.count()
+        assert qs == 2
 
 
 class TestProductModel:
-    def test_str_output(self, product_factory):
+    def test_str_method(self, product_factory):
         obj = product_factory(name="test_product")
         assert obj.__str__() == "test_product"
 
+    def test_fk_product_type_on_delete_protect(
+        self, product_type_factory, product_factory
+    ):
+        obj1 = product_type_factory()
+        product_factory(product_type=obj1)
+        with pytest.raises(IntegrityError):
+            obj1.delete()
 
-class TestProductLineModel:
-    def test_str_output(self, product_line_factory):
-        obj = product_line_factory(sku="12345")
-        assert obj.__str__() == "12345"
-
-    def clean_duplicate_order_values(self, product_line_factory, product_factory):
-        obj = product_factory()
-        product_line_factory(order=1, product=obj)
+    def test_name_max_length(self, product_factory):
+        name = "x" * 236
+        obj = product_factory(name=name)
         with pytest.raises(ValidationError):
-            product_line_factory(order=1, product=obj).clean()
+            obj.full_clean()
 
-
-class TestProductImageModel:
-    def test_str_method(self, product_image_factory, product_line_factory):
-        obj1 = product_line_factory(sku="12345")
-        obj2 = product_image_factory(order=1, product_line=obj1)
-        assert obj2.__str__() == "12345_img"
-
-    def test_alternative_text_field_length(self, product_image_factory):
-        alternative_text = "x" * 101
+    def test_slug_max_length(self, product_factory):
+        slug = "x" * 256
+        obj = product_factory(slug=slug)
         with pytest.raises(ValidationError):
-            product_image_factory(alternative_text=alternative_text)
+            obj.full_clean()
 
-    def test_duplicate_order_values(self, product_image_factory, product_line_factory):
-        obj = product_line_factory()
-        product_image_factory(order=1, product_line=obj)
+    def test_pid_length(self, product_factory):
+        pid = "x" * 11
+        obj = product_factory(pid=pid)
         with pytest.raises(ValidationError):
-            product_image_factory(order=1, product_line=obj).clean()
+            obj.full_clean()
+
+    def test_is_digital_false_default(self, product_factory):
+        obj = product_factory(is_digital=False)
+        assert obj.is_digital is False
+
+    def test_fk_category_on_delete_protect(self, category_factory, product_factory):
+        obj1 = category_factory()
+        product_factory(category=obj1)
+        with pytest.raises(IntegrityError):
+            obj1.delete()
+
+    def test_return_product_active_only_true(self, product_factory):
+        product_factory(is_active=True)
+        product_factory(is_active=False)
+        qs = Product.objects.is_active().count()
+        assert qs == 1
+
+    def test_return_product_active_only_false(self, product_factory):
+        product_factory(is_active=True)
+        product_factory(is_active=False)
+        qs = Product.objects.count()
+        assert qs == 2
